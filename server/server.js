fd0b4d9f5c9231b1fea0771b613c3bbe28629800
Colin Stodd <cbstodd@gmail.com>
2018-06-18 21:49:19 -0400

    Setup basic app files with MaterializeCSS and simple express server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
